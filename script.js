80324211bb47851e5433fea4cc7cc1135cc41080
Ashahar Jamal Khan <114991643+zanib2619@users.noreply.github.com>
2023-03-30 14:09:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ['Kahani Suno 2.0'];
 
 // Keep track of song
 let songIndex = 2;
